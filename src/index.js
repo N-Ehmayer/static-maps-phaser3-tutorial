2b6a5b31391a30fb40f40e692567d445597f266d
N-Ehmayer <nic.ehmayer92@gmail.com>
2018-10-08 22:38:07 -0600

    Added tutorial template to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import 'phaser';
-
-import { SimpleScene } from './scenes/simple_scene';
-
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
+const config = {
+  type: Phaser.AUTO, // Which renderer to use
+  width: 800, // Canvas width in pixels
+  height: 600, // Canvas height in pixels
+  parent: "game-container", // ID of the DOM element to add the canvas to
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
-new Phaser.Game(gameConfig);
+const game = new Phaser.Game(config);
+
+function preload() {
+  // Runs once, loads up assets like images and audio
+}
+
+function create() {
+  // Runs once, after all assets in preload are loaded
+}
+
+function update(time, delta) {
+  // Runs once per frame for the duration of the scene
+}
